3734f8fb00a4941677d2e821afa6f15bed801338
kedge95 <69055891+kedge95@users.noreply.github.com>
2024-11-12 20:27:56 -0500

    Update App.jsx
    Added HashRouter in hopes of getting navigation working on github pages

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from "./Pages/Header/Header";
 import Homepage from "./Pages/Homepage/Homepage";
 import Champions from "./Pages/Champions/Champions";
@@ -9,14 +9,14 @@ import ChampInfo from "./Pages/ChampInfo/ChampInfo";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" Component={Homepage} />
         <Route path="/Champions" Component={Champions} />
         <Route path="/ChampInfo/:id" Component={ChampInfo} />
       </Routes>
-    </>
+    </HashRouter>
   );
 }
 
